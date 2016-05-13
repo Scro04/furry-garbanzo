89fedf97b6b09dc406235cee732a6aa4fb081a0d
ChrisAluani <c.aluani@student.tugraz.at>
2016-05-13 10:39:34 +0200

    [CA, AS] Implemented first logic for program screen

--- TAO/www/js/Controller/programCtrl.js
+++ TAO/www/js/Controller/programCtrl.js
@@ -1,5 +1,25 @@
-app.controller('programCtrl', function($scope, $state) {
+app.controller('programCtrl', function ($scope, $state) {
 
-  console.log("programCtrl");
+    $scope.data = {};
+    $scope.$on("$ionicView.beforeEnter", function () {
+        $scope.data["Freitag"] = [];
+        $scope.data["Samstag"] = [];
+        $scope.data["Sonntag"] = [];
 
-})
+        for (var i = 0; i < 4; i++) {
+            var elem = {
+                type: 0,
+                title: "Titel des Vortrags Nr. 1 - Zusätzlicher Titel",
+                speaker: "Dr. Max Mustermann1",
+                start: "09:00",
+                end: "09:30"
+            };
+            $scope.data["Freitag"].push(elem);
+            $scope.data["Samstag"].push(elem);
+            $scope.data["Sonntag"].push(elem);
+
+        }
+
+        console.log($scope.data);
+    });
+});
