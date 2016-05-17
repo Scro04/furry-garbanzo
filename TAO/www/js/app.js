e14d5de040752c81729884d0dea2f604ab0e5900
Scro04 <a.schwaiger@student.tugraz.at>
2016-05-17 11:33:12 +0200

    merge into development

--- TAO/www/js/app.js
+++ TAO/www/js/app.js
@@ -7,102 +7,105 @@
 // 'starter.controllers' is found in controllers.js
 var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-  .run(function ($ionicPlatform) {
+.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
     });
-  })
+})
 
-  .config(function ($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
     // Set up the various states which the app can be in.
     // Each state's controller can be found in controllers.js
     $stateProvider.state('home', {
-      url: '/home',
-      templateUrl: 'templates/home.html',
-      controller: 'homeCtrl'
+        url: '/home',
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
     })
 
     // setup an abstract state for the tabs directive
-      .state('tab', {
+    .state('tab', {
         url: '/tab',
         abstract: true,
         templateUrl: 'templates/tabs.html'
-      })
+    })
 
-      // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-      .state('tab.program', {
+    .state('tab.program', {
         url: '/program',
         views: {
-          'tab-program': {
-            templateUrl: 'templates/tab-program.html',
-            controller: 'programCtrl'
-          }
+            'tab-program': {
+                templateUrl: 'templates/tab-program.html',
+                controller: 'programCtrl'
+            }
         }
-      })
+    })
 
-      .state('tab.referenten', {
-        url: '/referenten',
-        views: {
-          'tab-referenten': {
-            templateUrl: 'templates/tab-referenten.html',
-            controller: 'referentenCtrl'
-          }
-        }
-      })
-      .state('tab.anfahrt', {
-        url: '/anfahrt',
-        views: {
-          'tab-anfahrt': {
-            templateUrl: 'templates/tab-anfahrt.html',
-            controller: 'anfahrtCtrl'
-          }
-        }
-      })
+    .state('tab.referenten', {
+            url: '/referenten',
+            views: {
+                'tab-referenten': {
+                    templateUrl: 'templates/tab-referenten.html',
+                    controller: 'referentenCtrl'
+                }
+            }
+        })
+        .state('tab.anfahrt', {
+            url: '/anfahrt',
+            views: {
+                'tab-anfahrt': {
+                    templateUrl: 'templates/tab-anfahrt.html',
+                    controller: 'anfahrtCtrl'
+                }
+            }
+        })
 
-      .state('tab.info', {
-        url: '/info',
-        views: {
-          'tab-info': {
-            templateUrl: 'templates/tab-info.html',
-            controller: 'infoCtrl'
-          }
-        }
-      })
-      .state('tab.courseInfo', {
-        url: '/courseInfo/:id',
-        views: {
-          'tab-courseInfo': {
-            templateUrl: 'templates/courseDetail.html',
-            controller: 'courseInfoCtrl'
-          }
-        }
-      })
-      .state('tab.speakerDetail', {
-        url: '/speaker/:id',
-        views: {
-          'tab-speakerDetail': {
-            templateUrl: 'templates/speakerDetail.html',
-            controller: 'speakerDetailCtrl'
-          }
-        }
-      });
+    .state('tab.info', {
+            url: '/info',
+            views: {
+                'tab-info': {
+                    templateUrl: 'templates/tab-info.html',
+                    controller: 'infoCtrl'
+                }
+            }
+        })
+        .state('tab.courseInfo', {
+            url: '/courseInfo/:id',
+            views: {
+                'tab-courseInfo': {
+                    templateUrl: 'templates/courseDetail.html',
+                    controller: 'courseInfoCtrl'
+                }
+            }
+        })
+        .state('tab.speakerDetail', {
+            url: '/speaker/:id',
+            views: {
+                'tab-speakerDetail': {
+                    templateUrl: 'templates/speakerDetail.html',
+                    controller: 'speakerDetailCtrl'
+                }
+            }
+        });
 
     console.log("otherwise");
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/home');
 
-  });
+    //Set tab position
+    $ionicConfigProvider.tabs.position('bottom');
+
+});
