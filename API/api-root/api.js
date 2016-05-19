81ee0d772428e9d78daa066eec2c110a8c407404
valentin8251 <valentin.schreibmaier@gmail.com>
2016-05-19 10:44:06 +0200

    [VS, CA] changed api root url

--- API/api-root/api.js
+++ API/api-root/api.js
@@ -7,11 +7,8 @@ var app = express();
 
 logger.info("API-Started");
 
-connection.connection.query('SELECT 1', function(err, rows) {
-    // connected! (unless `err` is set)
-});
 
-app.use(require('./routes'));
+app.use("/tao", require('./routes'));
 
 app.listen(3000, function() {
     console.log('Listening on port 3000...')
