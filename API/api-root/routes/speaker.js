52e48afa5913cfa90ad7c3db6eb1d0d5cd854870
valentin8251 <valentin.schreibmaier@gmail.com>
2016-05-19 10:13:13 +0200

    [VS, CA] speaker route

--- API/api-root/routes/speaker.js
+++ API/api-root/routes/speaker.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const logger = require('../helpers/logger.js');
+var connection = require('../helpers/database.js');
 
 /**
  * @api {get} /speakers/ List of all speakers
@@ -11,7 +12,26 @@ const logger = require('../helpers/logger.js');
  * @apiSuccess {JSON} All speakers listed
  */
 router.get('/', function(req, res) {
-    res.send("Not yet implemented");
+
+    //SELECT * FROM appReferenten JOIN appReferentenBilder USING(id)
+    connection.connection.query('SELECT * FROM appReferenten JOIN appReferentenBilder USING(id)', function(err, rows) {
+        res.send(rows);
+    });
+});
+
+/**
+ * @api {get} /speakers/:id Get a Speaker
+ * @apiName GetASpeakers
+ * @apiGroup Speakers
+ *
+ *
+ * @apiSuccess {JSON} The requested speaker
+ */
+router.get('/:id', function(req, res) {
+    //SELECT * FROM appReferenten JOIN appReferentenBilder USING(id)
+    connection.connection.query('SELECT * FROM appReferenten JOIN appReferentenBilder USING(id) WHERE id=?', [req.param('id')], function(err, rows) {
+        res.send(rows);
+    });
 });
 
 module.exports = router;
