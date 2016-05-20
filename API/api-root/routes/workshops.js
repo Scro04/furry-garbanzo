a5f55de4b24b60d51e2d230d4ac0fda8f1a29c53
valentin8251 <valentin.schreibmaier@gmail.com>
2016-05-20 17:41:10 +0200

    [VS, CA] splitted time into start and end

--- API/api-root/routes/workshops.js
+++ API/api-root/routes/workshops.js
@@ -11,27 +11,44 @@ var connection = require('../helpers/database.js');
  *
  * @apiSuccess {JSON} All workshops listed
  */
-router.get('/', function(req, res) {
-    connection.connection.query('SELECT WorkshopId, appWorkshops.EinheitId, appWorkshops.Sprache, appWorkshops.TitelGER, appWorkshops.TitelENG, appWorkshops.BeschreibungGER, appWorkshops.BeschreibungENG, appWorkshops.Teil, Zeit, appWorkshops.AKId, appWorkshops.Handout, AKPunkte, appWorkshops.Reihung, appWorkshops.Seminarraum, ReferentId, appEinheiten.TagId ,appEinheiten.Uhrzeit AS EinheitZeit, appTage.Datum FROM appWorkshopsReferenten JOIN appWorkshops on WorkshopId = appWorkshops.id JOIN appEinheiten on appWorkshops.EinheitId = appEinheiten.id JOIN appTage on appTage.id = appEinheiten.TagId GROUP BY WorkshopId ORDER BY appEinheiten.TagId;', function(err, rows) {
+router.get('/', function (req, res) {
+    connection.connection.query('SELECT WorkshopId, appWorkshops.EinheitId, appWorkshops.Sprache, appWorkshops.TitelGER, appWorkshops.TitelENG, appWorkshops.BeschreibungGER, appWorkshops.BeschreibungENG, appWorkshops.Teil, Zeit, appWorkshops.AKId, appWorkshops.Handout, AKPunkte, appWorkshops.Reihung, appWorkshops.Seminarraum, ReferentId, appEinheiten.TagId ,appEinheiten.Uhrzeit AS EinheitZeit, appTage.Datum FROM appWorkshopsReferenten JOIN appWorkshops on WorkshopId = appWorkshops.id JOIN appEinheiten on appWorkshops.EinheitId = appEinheiten.id JOIN appTage on appTage.id = appEinheiten.TagId GROUP BY WorkshopId ORDER BY appEinheiten.TagId;', function (err, rows) {
         // ordering to match structure
         var response = {};
         const weekdays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"];
-        for (var i = 0; i < rows.length; i++)
-        {
+        for (var i = 0; i < rows.length; i++) {
             var date = new Date(rows[i]["Datum"]);
             date = weekdays[date.getDay()] + " " + date.getDate() + "." + (date.getMonth() + 1);
-            if(response[date] == undefined)
+            if (response[date] == undefined)
                 response[date] = [];
 
+            // splitting time
 
+            try {
+                if (rows[i]["Zeit"]) {
+                    rows[i]["type"] = 1
+                    var time = rows[i]["Zeit"];
+                    time = time.split(" - ");
+                    rows[i]["startZeit"] = time[0]
+                    rows[i]["endZeit"] = time[1]
+                    delete rows[i]["Zeit"];
+                } else {
+                    rows[i]["type"] = 0
+                    var time = rows[i]["EinheitZeit"];
+                    time = time.split("-");
+                    rows[i]["startZeit"] = time[0]
+                    rows[i]["endZeit"] = time[1]
+                    delete rows[i]["Zeit"];
+                }
 
-            if(rows[i]["Zeit"]){
-                rows[i]["type"] = 1
-            }else {
-                rows[i]["type"] = 0
-            }
+                var time = rows[i]["EinheitZeit"];
+                time = time.split("-");
+                rows[i]["startEinheit"] = time[0]
+                rows[i]["endEinheit"] = time[1]
+                response[date].push(rows[i])
 
-            response[date].push(rows[i])
+                delete rows[i]["EinheitZeit"];
+            }catch(err){}
 
         }
 
