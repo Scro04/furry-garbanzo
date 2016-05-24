1634e59c78bc4445e6aa09a4940ab968856a9a2e
ChrisAluani <c.aluani@student.tugraz.at>
2016-05-24 11:41:39 +0200

    [CA,AS] Added event creation for native calendar program screen

--- TAO/www/js/Controller/courseInfoCtrl.js
+++ TAO/www/js/Controller/courseInfoCtrl.js
@@ -1,77 +1,98 @@
-app.controller('courseInfoCtrl', function ($scope, $state, $stateParams, $ionicPopup, $ionicModal) {
+app.controller('courseInfoCtrl', function ($scope, $state, $stateParams, $ionicPopup, $ionicModal, $cordovaCalendar) {
 
-  $scope.speakerDataModel = undefined;
-
-
-  $ionicModal.fromTemplateUrl('templates/speaker-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-    $scope.modal.hide();
-  };
-  // Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
+    $scope.speakerDataModel = undefined;
 
 
-  $scope.showAlert = function() {
-    var alertPopup = $ionicPopup.alert({
-      title: 'Ein Fehler ist aufgetreten!',
-      template: 'Leider konnte der Speaker nicht gefunden werden, bitte versuchen Sie es später erneut, vielen Dank!'
+    $ionicModal.fromTemplateUrl('templates/speaker-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modal = modal;
     });
-  };
+    $scope.openModal = function () {
+        $scope.modal.show();
+    };
+    $scope.closeModal = function () {
+        $scope.modal.hide();
+    };
+    // Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function () {
+        $scope.modal.remove();
+    });
 
-  $scope.openSpeakerModal = function(referentId)
-  {
-    try {
-      for (var key in $scope.$root.speaker) {
-        if ($scope.$root.speaker.hasOwnProperty(key)) {
-          for (var speaker in $scope.$root.speaker[key]) {
-            if ($scope.$root.speaker[key].hasOwnProperty(speaker) && $scope.$root.speaker[key][speaker].id != null && $scope.$root.speaker[key][speaker].id == referentId) {
-              $scope.speakerDataModel = $scope.$root.speaker[key][speaker];
-              console.log($scope.speakerDataModel);
-              $scope.openModal();
+
+    $scope.showAlert = function () {
+        var alertPopup = $ionicPopup.alert({
+            title: 'Ein Fehler ist aufgetreten!',
+            template: 'Leider konnte der Speaker nicht gefunden werden, bitte versuchen Sie es später erneut, vielen Dank!'
+        });
+    };
+
+    $scope.openSpeakerModal = function (referentId) {
+        try {
+            for (var key in $scope.$root.speaker) {
+                if ($scope.$root.speaker.hasOwnProperty(key)) {
+                    for (var speaker in $scope.$root.speaker[key]) {
+                        if ($scope.$root.speaker[key].hasOwnProperty(speaker) && $scope.$root.speaker[key][speaker].id != null && $scope.$root.speaker[key][speaker].id == referentId) {
+                            $scope.speakerDataModel = $scope.$root.speaker[key][speaker];
+                            console.log($scope.speakerDataModel);
+                            $scope.openModal();
+                        }
+                    }
+                }
             }
-          }
+        } catch (error) {
+            console.log(error);
+            $scope.showAlert();
         }
-      }
     }
-    catch(error)
-    {
-      console.log(error);
-      $scope.showAlert();
-    }
-  }
 
-  $scope.getWorkshopsOfSpeaker = function(referentId)
-  {
-    $scope.workshopsSpeaker = [];
-    try {
-      for(workshopId in $scope.speakerDataModel.WorkshopId) {
-        for (var key in $scope.$root.program) {
-          if ($scope.$root.speaker.hasOwnProperty(key)) {
-            for (var program in $scope.$root.program[key]) {
-              if ($scope.$root.speaker[key].hasOwnProperty(program)) {
+    $scope.getWorkshopsOfSpeaker = function (referentId) {
+        $scope.workshopsSpeaker = [];
+        try {
+            for (workshopId in $scope.speakerDataModel.WorkshopId) {
+                for (var key in $scope.$root.program) {
+                    if ($scope.$root.speaker.hasOwnProperty(key)) {
+                        for (var program in $scope.$root.program[key]) {
+                            if ($scope.$root.speaker[key].hasOwnProperty(program)) {
 
 
-              }
+                            }
+                        }
+                    }
+                }
             }
-          }
+        } catch (error) {
+            console.log(error);
+            $scope.showAlert();
         }
-      }
     }
-    catch(error)
-    {
-      console.log(error);
-      $scope.showAlert();
+
+
+    $scope.addToCalendar = function () {
+
+        var notes = "Vortrag - ";
+        for (var i = 0; i < $scope.$root.currentProgram.speakers.length; i++) {
+            var speaker = $scope.$root.currentProgram.speakers[i];
+            notes += speaker.AkadgradPre + " " + speaker.Vorname + " " + speaker.Name + " | ";
+        }
+        console.log(notes);
+
+        if ($cordovaCalendar && window.cordova) {
+            $cordovaCalendar.createEventInteractively({
+                title: $scope.$root.currentProgram.TitelGER,
+                location: 'Vorklinik Uni Graz',
+                notes: notes,
+                startDate: new Date(2016, 8, 22, 09, 0, 0, 0, 0),
+                endDate: new Date(2016, 8, 22, 10, 30, 0, 0, 0)
+            }).then(function (result) {
+                console.log("Event created successfully");
+            }, function (err) {
+                console.error("There was an error: " + err);
+            });
+        }
+
+
     }
-  }
 
 });
